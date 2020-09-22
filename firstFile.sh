8608f50dc9cecea78c65bc8b30df1df157e1e158
vivekM43 <vivek@valuepitch.com>
2020-09-22 19:18:05 +0530

    adding if condition

--- firstFile.sh
+++ firstFile.sh
@@ -14,4 +14,14 @@ echo "Division two numbers"
 echo $(( x / y ))
 echo "Checking Modulus Remainder between two numbers"
 echo $(( x % y ))
-echo "We are editing this at server";
+#echo "We are editing this at server";
+echo "Lets Take new Number input";
+read z;
+sum=$(( x+y ))
+echo "checking if $z is bigger than $sum";
+if [ $z -gt $sum ];
+then
+echo "Baaaddaaaaaa";
+else
+echo "Chhhhooootttttaaaaa";
+fi
